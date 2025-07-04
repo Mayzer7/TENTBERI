ba46024ef69dd389ed2feafecceae16322b9495f
Mayzer <Tolik.Korzunov@yandex.ru>
2025-07-04 10:08:10 +0300

    Continue build header

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.header');
+    const headerContainer = document.querySelector('.header-container');
     const headerLogo = document.querySelector('.header-logo');
     const navs = document.querySelectorAll('.header-nav ul li a');
+    const navList     = document.querySelector('.header-nav ul');
     const headerPhone = document.querySelector('.header-phone');
     const getRequestBtn = document.querySelector('.get-request-btn');
 
@@ -10,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         headerPhone.classList.remove('add-border-scoll-up');
         getRequestBtn.classList.remove('add-border-scoll-up');
         navs.forEach(nav => nav.classList.remove('header-nav-scroll-up'));
+        headerContainer.classList.remove('scroll-up');
     };
 
     if (header) {
@@ -18,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Скрыть header при загрузке, если не вверху
         if (lastScroll > 0) {
             header.classList.add('header-hidden');
+            headerContainer.classList.remove('scroll-up');
             navs.forEach(nav => nav.classList.add('header-nav-scroll-up'));
             headerLogo.classList.add('header-logo-scroll-up');
             headerPhone.classList.add('add-border-scoll-up');
@@ -30,9 +34,12 @@ document.addEventListener('DOMContentLoaded', () => {
         window.addEventListener('scroll', () => {
             const currentScroll = window.pageYOffset;
 
+            console.log(currentScroll);
+
             if (currentScroll === 0) {
                 resetScrollUpStyles();
                 header.classList.remove('header-hidden');
+                navList.classList.remove('scroll-up-gap');
                 lastScroll = 0;
                 return;
             }
@@ -40,17 +47,20 @@ document.addEventListener('DOMContentLoaded', () => {
             if (currentScroll > lastScroll) {
                 // Скролл вниз — скрываем
                 header.classList.add('header-hidden');
-
+                
                 setTimeout(() => {
                     navs.forEach(nav => nav.classList.add('header-nav-scroll-up'));
+                    navList.classList.add('scroll-up-gap');
                 }, 200);
             } else if (currentScroll < lastScroll) {
                 // Скролл вверх — показываем
                 header.classList.remove('header-hidden');
+                headerContainer.classList.add('scroll-up');
                 headerLogo.classList.add('header-logo-scroll-up');
                 headerPhone.classList.add('add-border-scoll-up');
                 getRequestBtn.classList.add('add-border-scoll-up');
                 navs.forEach(nav => nav.classList.add('header-nav-scroll-up'));
+                navList.classList.add('scroll-up-gap');
             }
 
             lastScroll = currentScroll;
